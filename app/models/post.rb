c74fef9b7f68d7d7682d66405da8b09a6a3ad0c5
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-11-08 19:01:12 -0800

    setup post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,42 @@
 class Post < ApplicationRecord
+
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.create(robot_params)
+    redirect_to @post
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 end
